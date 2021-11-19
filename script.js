d5cb8486dbcb961e3a03409892641498bbe482db
IbrahimIsmayilov <90726322+IbrahimIsmayilov@users.noreply.github.com>
2021-11-19 14:48:53 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -12,6 +12,14 @@ for (num = 55; num >= 11; num -= 2) {
     console.log(num);
 }
 
-for (num = 1; num <= 500; num++) {
-    console.log(num);
+let total = 0
+for (num = 5; num <= 50; num++) {
+    total += num;
 }
+console.log(total);
+
+total = 0
+for (num = 10; num <= 100; num += 10) {
+    total += num;
+}
+console.log(total);
