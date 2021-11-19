8c4db72d693835fc628c5110fccc8d341d289b42
IbrahimIsmayilov <90726322+IbrahimIsmayilov@users.noreply.github.com>
2021-11-19 14:11:29 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// js
+// Loops Basics Asgn by Ibrahim Ismayilov
+
+for (num = 1; num <= 500; num++) {
+    console.log("I'm so happy!");
+}
+
+for (num = 12; num <= 800; num += 4) {
+    console.log(num);
+}
+
+for (num = 55; num >= 11; num -= 2) {
+    console.log(num);
+}
+
+for (num = 1; num <= 500; num++) {
+    console.log(num);
+}
